afc0581ef3bd0d0a2097388de9685537ae77c079
Andrew Lange <andrew.lange13@gmail.com>
2021-11-30 16:51:50 -0600

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -6,7 +6,13 @@ function helloWorld() {
 //// sayHello function
 
 function sayHello(input) {
-    if (input) {
+    if (input === true) {
+        return "Hello, World!"
+    } else if (input === " ") {
+        return "Hello, World!"
+    } else if (input) {
         return "Hello, " + input + "!"
+    } else {
+        return "Hello, World!"
     }
 }
--- tests.js
+++ tests.js
@@ -14,6 +14,7 @@ describe('helloWorld', function() {
     });
 });
 
+///sayHello
 
 describe('sayHello', function () {
     it('should be a defined function', function () {
@@ -22,13 +23,22 @@ describe('sayHello', function () {
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, Jane!', function () {
-        expect(sayHello("Jane")).toBe("Hello, Jane!");
-    });
-    it('should return "Hello, Alex!', function () {
+    it('should return "Hello, Alex!"', function () {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
-    it('should return "Hello, Pat!', function () {
+    it('should return "Hello, Pat!"', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return "Hello, World!"', function () {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!"', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!"', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!"', function () {
+        expect(sayHello(" ")).toBe("Hello, World!");
     });
 });
