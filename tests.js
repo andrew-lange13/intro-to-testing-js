5d14c5b832390626033e6fa7135143fc6eabc402
Andrew Lange <andrew.lange13@gmail.com>
2021-11-30 15:47:39 -0600

    adds the second test to exercise

--- tests.js
+++ tests.js
@@ -19,4 +19,7 @@ describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello()).toBeDefined();
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
 });
